3ac4d52cf0b0e63338171efb505e9d25b78ceab0
Nitramcr <martingolramirez@gmail.com>
2023-12-15 20:12:23 -0300

    Ready to deploy.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Hero } from './views/Hero'
+import { Services } from './views/Services'
+import { Products } from './views/Products'
+import { Reference } from './views/Reference'
+import { Care } from './views/Care'
+import { Footer } from './views/Footer'
+
+export function App() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Products />
+      <Reference />
+      <Care />
+      <Footer />
+    </>
+  )
+}
---
+++ src/views/Hero.jsx
@@ -0,0 +1,115 @@
+// local imports
+import heroImage from '../assets/hero-image.svg'
+import logo from '../assets/logo.svg'
+import cartIcon from '../assets/cart-icon.svg'
+import searchIcon from '../assets/search-icon.svg'
+import { heroTitle, heroSubtitle } from '../data'
+
+import { FadeIn } from '../components/FadeIn'
+// import NavLink from '../components/NavLink'
+
+import { useState } from 'react'
+import { NavLink } from '../components/NavLink'
+import { HiMenuAlt3 as MenuIcon } from 'react-icons/hi'
+import { AiOutlineClose as CloseIcon } from 'react-icons/ai'
+
+export function Hero() {
+  const [showMobileMenu, setShowMobileMenu] = useState(false)
+
+  return (
+    <div
+      id='hero'
+      className='flex flex-col items-center h-screen relative'
+      style={{
+        background: `url(${heroImage})`,
+        backgroundPosition: 'bottom',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+      }}
+    >
+      <div className='flex items-center justify-between relative w-full max-w-[1490px] pt-10 mx-auto px-10'>
+        <img src={logo} alt='Logo' />
+        <ul className='hidden md:flex items-center gap-10 lg:gap-[68px]'>
+          <NavLink to='services'>Services</NavLink>
+          <NavLink to='products'>Shop</NavLink>
+          <NavLink to='reference'>Reference</NavLink>
+          <NavLink to='care'>Care</NavLink>
+        </ul>
+
+        <img
+          src={cartIcon}
+          alt='Cart icon'
+          className='hidden md:block cursor-pointer'
+        />
+        <MenuIcon
+          size={30}
+          className='block md:hidden cursor-pointer text-white'
+          onClick={() => setShowMobileMenu((prev) => !prev)}
+        />
+
+        <div
+          className={`block md:hidden w-full fixed ${
+            !showMobileMenu ? '-top-[420px]' : 'top-0'
+          } left-0 bg-[#dde0e5] h-[420px] transition-all duration-[500ms] shadow-xl z-10 py-8 px-12 rounded-b-xl`}
+        >
+          <CloseIcon
+            size={25}
+            className='absolute top-5 right-5 cursor-pointer'
+            onClick={() => setShowMobileMenu(false)}
+          />
+
+          <ul className='pt-[20px] items-center flex flex-col gap-8'>
+            <NavLink to='hero' mobileMenu>
+              Home
+            </NavLink>
+            <NavLink to='services' mobileMenu>
+              Services
+            </NavLink>
+            <NavLink to='products' mobileMenu>
+              Shop
+            </NavLink>
+            <NavLink to='reference' mobileMenu>
+              Reference
+            </NavLink>
+            <NavLink to='care' mobileMenu>
+              Care
+            </NavLink>
+          </ul>
+
+          <img
+            src={cartIcon}
+            alt='Cart icon'
+            className='mt-8 mx-auto cursor-pointer'
+          />
+        </div>
+      </div>
+
+      <FadeIn delay={0.2} direction='down' padding fullWidth>
+        <h1 className='mt-[90px] text-center text-5xl leading-tight xs:text-[64px] text-white max-w-[1050px]'>
+          {heroTitle}
+        </h1>
+      </FadeIn>
+      <FadeIn delay={0.4} direction='down' padding fullWidth>
+        <h5 className='mt-6 text-center text-lg xs:text-xl text-white max-w-[500px]'>
+          {heroSubtitle}
+        </h5>
+      </FadeIn>
+      <FadeIn delay={0.2} direction='up' padding fullWidth>
+        <div className='relative w-full xs:w-[460px] mt-11'>
+          <input
+            type='text'
+            placeholder='Search'
+            className='rounded-full w-full pl-6 pr-[68px] py-4 bg-primary outline-none text-white text-base xs:text-lg placeholder-white'
+          />
+          <img
+            src={searchIcon}
+            alt='Search Icon'
+            className='absolute top-2/4 -translate-y-2/4 right-3 h-11 w-11 cursor-pointer'
+          />
+        </div>
+      </FadeIn>
+
+      <div className='absolute h-[50px] xs:h-[150px] bottom-0 w-full bg:[linear-gradient(180deg,_#ffff00_0%,_#FFF_%100)]'></div>
+    </div>
+  )
+}
